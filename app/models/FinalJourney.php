077020d092c8e704b91837513f1366af2dbc76b4
prateekchandan <prateekchandan5545@gmail.com>
2015-02-27 03:06:29 +0530

    hidden fields

--- app/models/FinalJourney.php
+++ app/models/FinalJourney.php
@@ -4,6 +4,7 @@
 class FinalJourney extends Eloquent{
 
 	protected $table = 'created_journeys';
+	protected $hidden = array('id','created_at','updated_at');
 
 	/**
 	 * The attributes excluded from the model's JSON form.
