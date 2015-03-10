50652b3e0e883c228eace3cac72512686e0396cf
prateekchandan <prateekchandan5545@gmail.com>
2015-03-10 17:30:57 +0530

    cost correct

--- app/models/CostCalc.php
+++ app/models/CostCalc.php
@@ -2,7 +2,8 @@
 class CostCalc{
 	public static function calc($distance,$time=0,$type=0){
 		if($type==0){
-			return 150 + max(array(0,$distance-4000)) * 0.01;
+			//return 150 + max(array(0,$distance-4000)) * (21/1000);
+			return min(array(150,$distance/1000.0 * 21));
 		}
 	}
 };
