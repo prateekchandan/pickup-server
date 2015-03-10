690116335177ece90b290d866ad62ae7496ae47f
prateekchandan <prateekchandan5545@gmail.com>
2015-03-10 17:25:48 +0530

    cost calc function

--- app/models/CostCalc.php
+++ app/models/CostCalc.php
@@ -1,6 +1,6 @@
 <?php
 class CostCalc{
-	public static calc($distance,$time=0,$type=0){
+	public static function calc($distance,$time=0,$type=0){
 		if($type==0){
 			return 150 + max(array(0,$distance-4000)) * 10;
 		}
