53b98fbe8f34afe629978f3294b96217e894aa11
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-08-01 14:49:46 +0530

    bug fix

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -238,8 +238,8 @@ class HomeController extends BaseController {
 		for ($i=1;$i<=3;$i++)
 		{
 			$match_array = self::find_mates($journey_id,$i,1,False)['mates'];
-			$log_data = "Matching route ".$i." of journey_id ".$journey_id." with groups...\n";
-			$log_data+=print_r($match_array,true);
+			self::log_matches("Matching route ".$i." of journey_id ".$journey_id." with groups...\n");
+			$log_data=print_r($match_array,true);
 			self::log_matches($log_data);
 			$match = $match_array[0];
 			if (!is_null($match) && $match->match_amount>$best_match_value)
@@ -261,8 +261,8 @@ class HomeController extends BaseController {
 				for ($j=1;$j<=3;$j++)
 				{
 					$match_array = self::find_mates($journey_id,$i,$j,True)['mates'];
-					$log_data = "Matching route ".$i." of journey_id ".$journey_id." with lonely group journey ".$j."...\n";
-					$log_data+=print_r($match_array,true);
+					self::log_matches("Matching route ".$i." of journey_id ".$journey_id." with lonely group journey ".$j."...\n");
+					$log_data=print_r($match_array,true);
 					self::log_matches($log_data);
 					$match = $match_array[0];
 					if (!is_null($match) && $match->match_amount>$best_match_value)
