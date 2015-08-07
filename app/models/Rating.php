176bbe33af04dc5a46134538af0afe9054f9cfa0
Kalpesh Krishna <kalpeshk2011@gmail.com>
2015-08-08 02:51:55 +0530

    Rating API added, modify location API added

---
+++ app/models/Rating.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Rating extends Eloquent{
+
+	protected $table = 'ratings';
+	//protected $hidden = array('group_id','created_at','updated_at');
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+
+
+
+}
