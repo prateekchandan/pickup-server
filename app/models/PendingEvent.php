f96a5bb11fd06a06d4cbe93da3cea3989c48c7a1
Kalpesh Krishna <kalpeshk2011@gmail.com>
2015-08-11 21:19:59 +0530

    events table bug fix

---
+++ app/models/PendingEvent.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class PendingEvent extends Eloquent{
+
+	protected $table = 'events';
+	//protected $hidden = array('registration_id','created_at','updated_at','remember_token','second_name');
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+
+
+
+}
