8d9df035ec6a52db732a03a0473a433ba31b8ebe
Kalpesh Krishna <kalpeshk2011@gmail.com>
2015-09-02 03:01:13 +0530

    UserController doc in progress

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -178,64 +178,91 @@ class UserController extends BaseController {
 		}
 		}
 	}
+
+
+	/**
+	 * Primary function to add new users into the user database.
+	 *
+	 * Before any API can be used, we have to add a user and obtain his/her user_id.<br>
+	 * Used by route :- <br>
+	 * Route::post('add_user', array('as' => 'user.add', 'uses' => 'UserController@add'));
+	 *
+	 * Parameters required by route :- <br>
+	 * <b>name</b> <i>string</i> - Name of user to be added.<br>
+	 * <b>age</b> <i>string</i> - age of user to be added.<br>
+	 * <b>fbid</b> <i>string</i> - Facebook ID of user.<br>
+	 * <b>phone</b> <i>string</i> - Phone Number of the user. <br>
+	 * <b>email</b> <i>string</i> - email ID of user.<br>
+	 * <b>gender</b> <i>string</i> - Male or Female type of user<br>
+	 * <b>company</b> <i>string</i> - Company of user<br>
+	 * <b>device_id</b> <i>string</i> - Android phone device ID<br>
+	 * <b>mac_addr</b> <i>string</i> - MAC address of NIC used by phone<br>
+	 * <b>company_email</b> <i>string</i> - (optional parameter) Company offical email ID
+	 *
+	 * @return mixed[] This array contains the user_id of the user.
+	*/
 	public function add()
 	{
-		$requirements = ['fbid' ,'age','phone','company',
-						 'name' , 'email' , 'gender' , 'device_id','mac_addr'];
+		$requirements = ['fbid' ,'age','phone','company','name' , 
+						'email' , 'gender' , 'device_id','mac_addr'];
 		$check  = self::check_requirements($requirements);
 		if($check)
 			return Error::make(0,100,$check);
 
-		$user = User::where('fbid' , '=', Input::get('fbid'))->orWhere('email' , '=' , Input::get('email'))->first();
-		if(!is_null($user)){
+		// Checking whether user exists using his email/facebook ID.
+		$user = User::where('fbid' , '=', Input::get('fbid'))->
+					  orWhere('email' , '=' , Input::get('email'))->
+					  first();
+		if (!is_null($user)) {
+			// Case when the user changes his/her device.
 			$user->device_id = Input::get('device_id');
 			$user->save();
 			return Error::success("User Already Present" , array("user_id" => $user->id));
 		}
+
+		// Adding new user if old user is not found.
 		$user = new User;
 		$user->fbid = Input::get('fbid');
 		$user->age = Input::get('age');
 		$user->phone = Input::get('phone');
 		$user->company = Input::get('company');
 		$user->first_name = Input::get('name');
-		$user->second_name = "";//Input::get('second_name');
+		$user->second_name = "";
 		$user->email =  Input::get('email');
 		$user->gender = Input::get('gender');
 		$user->device_id = Input::get('device_id');
-		$user->registration_id = "";
+		$user->registration_id = ""; //Comes under gcm_add()
 		$user->mac_addr = Input::get('mac_addr');
-		$user->current_pos="19.1336,72.9154";
+		$user->current_pos="19.1336,72.9154"; // Updated in the periodic route.
 		if (Input::has('company_email'))
 			$user->company_email = Input::get('company_email');
-		/*$user->home_location=;
-		$user->home_text=Input::get('home_text');
-		$user->office_location=Input::get('office_location');
-		$user->office_text=Input::get('office_text');
-		$user->leaving_office=Input::get('leaving_office');
-		$user->leaving_home=Input::get('leaving_home');*/
-		/*try {
-		$user->home_to_office=self::getPath($user->home_location,$user->office_location);
-		$user->office_to_home=self::getPath($user->office_location,$user->home_location);
-		$json_home_office=json_decode($user->home_to_office)->routes[0];
-		$json_office_home=json_decode($user->office_to_home)->routes[0];
-		$user->home_to_office=json_encode(Graining::get_hashed_grid_points(json_encode($json_home_office)));
-		$user->office_to_home=json_encode(Graining::get_hashed_grid_points(json_encode($json_office_home)));
-		}
-		catch (Exception $e)
-		{
-			return Error::make(1,21);
-		}
-		$user->path_distance=$json_home_office->legs[0]->distance->value;
-		$user->path_time=$json_home_office->legs[0]->duration->value;*/
+
+		// Saving user object created.
 		try {
 			$user->save();
-			//$user->id = 10;
-			//$this->sendmail($user);
 			return Error::success("User successfully Added" , array("user_id" => $user->id));
-		} catch (Exception $e) {
+		} 
+		catch (Exception $e) {
 			return Error::make(101,101,$e->getMessage());
 		}
 	}
+
+
+	/**
+	 * Function to test user's existence on database solely based on Facebook ID.
+	 *
+	 * The HTTP request contains the facebook ID. The function is run every time
+	 * the app is reinstalled. It returns the user data if he had registered earlier.
+	 * 
+	 * Used by route :- <br>
+	 * Route::get('user_exists','UserController@check_existence');
+	 * 
+	 * Parameters required by route :- <br>
+	 * <b>fbid</b> <i>string</i> - Facebook ID of user whose existence we wish to check.
+	 *
+	 * @return mixed[] Returns user data if user is present. Else returns NULL in that
+	 * field.
+	*/
 	public function check_existence()
 	{
 		$requirements = ['fbid'];
@@ -244,6 +271,7 @@ class UserController extends BaseController {
 			return Error::make(0,100,$check);
 
 		$user = User::where('fbid' , '=', Input::get('fbid'))->first();
+
 		$final_data = array("user_present"=>0,"user_data"=>$user);
 		if (!is_null($user))
 		{
@@ -252,14 +280,28 @@ class UserController extends BaseController {
 			$final_data['user_data']->office_to_home=NULL;
 		}
 		return Error::success("Successfully logged in",$final_data);
-		//return $final_data;
-		
 	}
+
+
+	/**
+	 * Helper function to get path between two points.
+	 *
+	 * Used by all functions of this class. Returns Google Maps path
+	 * by contacting Google Direction API.
+	 * Inputs must be comma separated (latitude,longitude) type points.
+	 *
+	 * @param string $start comma separated latitude,longitude string
+	 * @param string $end comma separated latitude,longitude string
+	 * @return mixed[] raw path returned by Directions API
+	*/
 	public function getPath($start,$end)
 	{
 		$path=file_get_contents("https://maps.googleapis.com/maps/api/directions/json?origin=$start&destination=$end");
 		return $path;
 	}
+
+
+	
 	public function gcm_add(){
 		$requirements = ['reg_id' , 'user_id'];
 		$check  = self::check_requirements($requirements);
